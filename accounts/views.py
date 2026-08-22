51436f4de7904ed9f6a24ed5d1aa985c0867cdc4
Guddesh H Patil <patil.hc1@gmail.com>
2026-08-22 17:29:11 +0530

    Build Zarko landing page and login routing

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, "accounts/index.html")
+
+
+def login_view(request):
+    return render(request, "accounts/login.html")
